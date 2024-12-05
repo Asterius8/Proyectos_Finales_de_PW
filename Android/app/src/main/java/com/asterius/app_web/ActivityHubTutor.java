aaaf2c8917189b91ae3a69fcd45dcd800f79ef07
Asterius8 <oscar.vg@tecjerez.edu.mx>
2024-12-04 19:18:45 -0600

    Diseño de activity tutor hub

---
+++ Android/app/src/main/java/com/asterius/app_web/ActivityHubTutor.java
@@ -0,0 +1,15 @@
+package com.asterius.app_web;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ActivityHubTutor extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+    }
+
+}
